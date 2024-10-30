9e8927bf684e6db9319aebae81fd5c8f7f124fce
vanguard-gpt <vanguard09@naver.com>
2024-10-30 10:53:47 +0900

    first commit

---
+++ YT-aouth-test/src/main/java/com/example/YT_aouth_test/YtAouthTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.YT_aouth_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtAouthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtAouthTestApplication.class, args);
+	}
+
+}
---
+++ YT-aouth-test/src/test/java/com/example/YT_aouth_test/YtAouthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YT_aouth_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtAouthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
